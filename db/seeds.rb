1c05a3eef73d3d0995e50c31762dd7f04ae2ea60
mike <mikemcfaddin@gmail.com>
2020-03-09 10:46:41 -0400

    finishes seeds for posts in all 6 categories, 4 posts each

--- db/seeds.rb
+++ db/seeds.rb
@@ -193,11 +193,8 @@ Post.create!([
     4. Scoop a healthy amount of the chickpea mixture (about ½ cup) onto one slice of bread, add desired toppings and top with second slice of bread. Repeat for additional sandwiches.", 
     user_id: 30, 
     category_id: 1
-  }
-])
-
-#Category Waste
-Post.create!([
+  },
+#Category Waste------------------------------------------------------------
   {
     title: "Get To Know The Rules of Recycling", 
     body: "Don’t be intimidated by the complicated symbols and sorting rules. Navigating the recycling system is actually quite simple once you do a little research. Since recycling standards vary from place to place, find out the specific rules for your area. While it’s better to implement reusable packaging in your home wherever possible, recycling is the next best option and it will prevent excess waste from heading for the landfill. I have attached the rules from my local hauling company as an example. Yours will most likely vary.", 
@@ -222,17 +219,11 @@ Post.create!([
     user_id: 31, 
     category_id: 2
   },
-])
-
-
-
-
-#Category Energy
-Post.create!([
+#Category Energy------------------------------------------------------------
   {
     title: "Adjust Your Day To Day Behaviors", 
     body: "To reduce energy consumption in your home, you do not necessarily need to go out and purchase energy efficient products. Energy conservation can be as simple as turning off lights or appliances when you do not need them. You can also use energy-intensive appliances less by performing household tasks manually, such as hang-drying your clothes instead of putting them in the dryer, or washing dishes by hand. The behavior adjustments that have the highest potential for utility savings are turning down the heat on your thermostat in the winter and using your air conditioner less in the summer. Heating and cooling costs constitute nearly half of an average home’s utility bills, so these reductions in the intensity and frequency of heating and cooling offer the greatest savings. These are just a few things anyone can do. Start changing your day to day behaviors one at a time to make a big change down the road.", 
-    user_id: 26, 
+    user_id: 9, 
     category_id: 3
   },
   {
@@ -259,19 +250,117 @@ Post.create!([
     body: "Here is a great document about how to perform your own home energy audit. I have looked into this quite a bit. There are lots of bits and pieces around the internet on how to do this. This  document has the most info all in one place. Its also probably the best info I have found as well. Good luck! https://www.seattle.gov/Documents/Departments/OSE/GreenHomeGuide-FYIenergyaudit.pdf", 
     user_id: 28, 
     category_id: 3
-  }
-])
-
-
-#Category Transportation
-Post.create!([
+  },
+#Category Transportation------------------------------------------------------------
   {
-    title: "Adjust Your Day To Day Behaviors", 
-    body: "To reduce energy consumption in your home, you do not necessarily need to go out and purchase energy efficient products. Energy conservation can be as simple as turning off lights or appliances when you do not need them. You can also use energy-intensive appliances less by performing household tasks manually, such as hang-drying your clothes instead of putting them in the dryer, or washing dishes by hand. The behavior adjustments that have the highest potential for utility savings are turning down the heat on your thermostat in the winter and using your air conditioner less in the summer. Heating and cooling costs constitute nearly half of an average home’s utility bills, so these reductions in the intensity and frequency of heating and cooling offer the greatest savings. These are just a few things anyone can do. Start changing your day to day behaviors one at a time to make a big change down the road.", 
-    user_id: 26, 
+    title: "Electric Vehicle Maintenance Vs. Gas Vehicle", 
+    body: "Gas-powered automobiles require replacing parts that go bad over time. Electric vehicles are different because they do not need as many components to operate. For instance, electric motors only have one moving part while engines in traditional automobiles contain dozens. Items that do not need to be replaced in electric vehicles include oil, fan belts, air filters, timing belts, head gaskets, cylinder heads and spark plugs. Thus, keeping an electric car running costs less when compared to conventional automobiles. Just a few things I thought of when someone asked me about my maintenance schedule.", 
+    user_id: 7, 
     category_id: 4
+  },
+  {
+    title: "Basics Of An Electric Bike", 
+    body: "Here is a brief intro on how electric bikes work. If you have dynamo-powered bicycle lights, you already own an electric-powered bicycle! Consider: as you pump your legs up and down on the pedals, you make the wheels rotate. A small dynamo (generator) mounted on the rear wheel produces a tiny current of electricity that keeps your back safety lamp lit in the dark. Now suppose you could run this process backward. What if you removed the lamp and replaced it with a large battery. The battery would kick out a steady electric current, driving the dynamo in reverse so that it spun around like an electric motor. As the dynamo/motor turned, it would rotate the tire and make the bike go along without any help from your pedaling. Hey presto: an electric bike! It may sound a bit far-fetched, but this is more or less exactly how electric bikes work. I currently have one and love it for my commute. It may not be the best choice for every ride but give one a try. I have an image attached if you have never seen one of those dynamo generators. https://i.pinimg.com/originals/64/ea/51/64ea51176bb5789c0ff2193d508bb4bf.jpg", 
+    user_id: 33, 
+    category_id: 4
+  },
+  {
+    title: "Top Carpooling Ground Rules", 
+    body: "If you are thinking of getting into carpooling here are some of my top ground rules. These are the key things that I think need to be discussed before getting in on a carpool. Good luck!
+
+      1. Where will the designated pick-up point be? Some car pools have door-to-door service, while others prefer to meet at a convenient church or grocery store parking lot.
+      2. Establish smoking/non-smoking policies.
+      3. Is eating/drinking allowed?
+      4. Which station will you listen to on the radio? News channels are usually great ideas for updates on traffic, sports, stocks, and weather.
+      5. Will there be any unscheduled stops?
+      6. Who will be your alternate drivers in cases of illness or emergency?
+      7. How long will you wait for someone if he or she is late? Three to five minutes is usually recommended.
+      8. How many days a week will you share the ride to work? There are car pools operating every weekday and some that run just once or twice a week.", 
+    user_id: 10, 
+    category_id: 4
+  },
+  {
+    title: "My Experience with Carpool Waze.", 
+    body: "So I gave Carpool Waze a try. Signed up a couple weeks back and finally gave my first ride today.
+      
+      PROS:
+      - Easy sign up. You don't have to submit any documents like proof of insurance. You fill up a general form via app and you are done. 
+      - Any car qualifies. They don't even ask for a vehicle inspection or what car you are driving. 
+      - Before accepting a trip, you know exactly where you are going to be picking up the rider and where you will be dropping them off. 
+      - No stupid trade dress on your windshield. 
+      - Google/Waze doesn't take a cut of your pay. They are considered a non-profit program to reduce traffic and help the environment. 
+      
+      CONS:
+      - Pay is very low compared to Uber/Lyft. 
+      - Not enough requests. It's best to plan a trip and offer a ride in advance so you get a trip request. 
+      - You get paid monthly, every 15th of the month. 
+      
+      Final verdict: It is a great tool for what it is, carpooling. I picked a nice young lady that lives a block away from where I live and dropped her off 2 blocks before arriving to my workplace. It's destination trip matching was spot on because I did not drive out of my way to drive to work. Total pay for a 12 mile trip, $4.00- again, it's an app to help cover gas expenses for driving to destinations that were already going to in the first place.", 
+    user_id: 4, 
+    category_id: 4
+  },
+#Category Water------------------------------------------------------------
+  {
+    title: "Bottled Water Is Just Tap Water!", 
+    body: "If you're still insisting bottled water is worth the cost, consider the fact that you might just be drinking regular old tap water out of that fancy bottle anyway. In 2007, Pepsi made a startling announcement. They were changing their labels to be more transparent about where their bottled water came from, because their Aquafina was nothing more or less than tap water. If you happened to read an Aquafina label before that, you would have seen 'Bottled at the source P.W.S.' That's a fancy way of saying it's bottled at a Public Water Source. It's poor labeling and shady marketing at best. Pepsi isn't alone here. Coca-Cola's Dasani is also essentially tap water. It comes from a public water source, as do some of Nestle's bottled water products. If it's natural spring water or well water you think you're getting when you open that bottle, you might find you've been mistaken. Always carry your own water bottle.", 
+    user_id: 31, 
+    category_id: 5
+  },
+  {
+    title: "Interesting Water Facts", 
+    body: "Here is some info I came across. Really makes you think about your water usage.
+    Our world’s population, along with our need for clean freshwater, is growing, but our water supply is not. According to the United Nations, about 2 billion people are currently living in areas facing serious water scarcity. By 2040, it is estimated that one in four of the world’s children under 18, or 600 million, will be living in areas of extremely high water stress. Our water enables us to run our businesses, protect our communities and keep ourselves alive, so it is critical that we work to reduce water usage in our everyday lives.  
+
+    Do you know how much water you use each day? Most people have no idea! People in the U.S. use approximately 100 gallons of fresh, drinkable water per day for bathing, drinking, flushing, cleaning and yard care. And this number only takes into account the amount of water we use directly every day.  
+
+    Everything we buy, the energy we use and the food we eat requires water to produce. In fact, Americans’ actual water “footprint” – the amount of water it takes to produce our food, energy, clothes and more – is about 2,000 gallons of water each day. We consume about 95 percent of the water we use without ever seeing it. Purchasing recycled goods, carpooling with friends and eating locally grown veggies all will help to reduce our water footprint.", 
+    user_id: 32, 
+    category_id: 5
+  },
+  {
+    title: "How Much Water", 
+    body: "Found this interesting blurb on how much water it takes to make some of my favorite beverages. Here is the link, https://www.npr.org/sections/thesalt/2013/10/28/241419373/how-much-water-actually-goes-into-making-a-bottle-of-water. I also just grabbed some lines here.
+
+      The International Bottled Water Association, ever sensitive to criticism that it's wasting precious resources, has commissioned its first ever study to figure out how much water goes into producing one liter. The results, released this month, show that for North American companies, it takes 1.39 liters to make one liter of water. That's less than the global averages of a liter of soda, which requires 2.02 liters of water. A liter of beer, meanwhile, needs 4 liters of water, wine demands 4.74 liters. Hard alcohol, it turns out, is the greediest, guzzling 34.55 liters of water for every liter.", 
+    user_id: 26, 
+    category_id: 5
+  },
+  {
+    title: "My Water Saving Landscape", 
+    body: "I decided to go with a water saving landscape.  One of the best things I have ever done! I picked drought tolerant plants and gravel/rock accents. This isn’t rocket science. Go to a local nursery and find out what kind of plants are drought resistant and need the lest amount of work to keep up. Even better if those plants are native to the area. Snap a photo of you property from google earth and start drawing out want the final layout to look like. You can even do it in multiple steps over a number of weekends. My lawn might need some weed pulling here and there but thats about it. No more lawn mower or yard service. Good luck!", 
+    user_id: 11, 
+    category_id: 5
+  },
+  #Category Social------------------------------------------------------------
+  {
+    title: "The Uraban Garden Initiative", 
+    body: "The TUGI organization is great! I decided I should share a little bit about it here to help spread the word.
+
+      Why Urban Gardening? As explained by the RUAF Foundation, 'Urban agriculture plays an important role in enhancing urban food security since the costs of supplying and distributing food to urban areas based on rural production and imports continue to increase, and do not satisfy the demand, especially of the poorer sectors of the population. Next to food security, urban agriculture contributes to local economic development, poverty alleviation and social inclusion of the urban poor and women in particular, as well as to the greening of the city and the productive reuse of urban wastes.' In an effort to solve these community issues through something that I love doing, The Urban Garden Initiative was born. At TUGI, we believe that we can not tackle these issues alone though, instead we aim to provide the youth the knowledge and skillset to take on these challenges together.", 
+    user_id: 31, 
+    category_id: 6
+  },
+  {
+    title: "Environmental and Social Justice Organizations", 
+    body: "So I have a fairly focused view of the environment and what I feel like people should be doing to do their part for keep things green. Social justice is also one of my passions. I just happened to come across this website with a great list of organizations that cover both of these issues. We all have a lot of things to check out but please take a moment to check out this site when you have time. http://www.globalstewards.org/environmental-organizations.htm", 
+    user_id: 27, 
+    category_id: 6
+  },
+  {
+    title: "They Want the Bottles Back", 
+    body: "Has anyone checked out this site from the American Beverage Association? It discusses how the industry wants all the bottles it makes back so they can recycling them. I think its a good step. Not sure they are actually coming up with new and more efficient ways to get them back. Lets see what happens shall we! https://www.innovationnaturally.org/plastic/?gclid=Cj0KCQjw0pfzBRCOARIsANi0g0v5u-gTg34FQW2kf5QX7iVUMMP2qu61-zBorodVkwS2qAlqmo7kSUcaAhMbEALw_wcB", 
+    user_id: 9, 
+    category_id: 6
+  },
+  {
+    title: "Help Clean Up", 
+    body: "If you don’t know where to start helping just clean up! While this might not seem like the most innovative ways to bring about environmental change, it’s one of the most rewarding. Since there are no government agencies regularly responsible for cleaning trash from a river, it will usually only happen if a community gets involved. Removing trash can make a river safer for wildlife and humans and improve water quality while protecting aquatic habitats. According to the EPA, as many as 2 million seabirds are killed every year due to debris ingestion and entanglement. Another 100,000 marine mammals are killed annually from the same cause. While ultimately preventing pollution in the first place is a better solution, most of us don’t want to sit around and wait while our waterways become trash pits. You can join a locally-sponsored cleanup or find a National River Cleanup through American Rivers. Or look for your local Riverkeeper chapter and sign up!", 
+    user_id: 30, 
+    category_id: 6
   }
 ])
+
+
 
 # Image.create!([
 #   {

61fe6127e94482aa13a604d9d78b7c8871af396c
mike <mikemcfaddin@gmail.com>
2020-03-23 13:01:11 -0400

    makes adjustments to recipe controller and index view

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,7 +1,7 @@
 class Api::RecipesController < ApplicationController
 
   def index
-    response = HTTP.get("https://api.spoonacular.com/recipes/random?tags=vegan,main_course&number=1&apiKey=#{Rails.application.credentials.spoonacular[:api_key]}")
+    response = HTTP.get("https://api.spoonacular.com/recipes/random?tags=vegetarian,dinner&number=1&apiKey=#{Rails.application.credentials.spoonacular[:api_key]}")
     @recipes = response.parse["recipes"]
     render "index.json.jb"
   end

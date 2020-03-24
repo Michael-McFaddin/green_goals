29eff81723c8c572c1d415535378cc4b04d06bce
mike <mikemcfaddin@gmail.com>
2020-03-24 18:53:40 -0400

    fixes users controller errors

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,7 +14,7 @@ class Api::UsersController < ApplicationController
     if @user.save
       render "show.json.jb"
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
 

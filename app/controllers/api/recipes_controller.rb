0141014087ad5b376470acb1f2a407a9c533d5fa
mike <mikemcfaddin@gmail.com>
2020-03-25 23:53:53 -0400

    add seed dump gem, dumped database, finishes up recipes controller logic for spoonacular query

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,9 +1,15 @@
 class Api::RecipesController < ApplicationController
 
   def index
-    response = HTTP.get("https://api.spoonacular.com/recipes/random?tags=vegetarian,dinner&number=1&apiKey=#{Rails.application.credentials.spoonacular[:api_key]}")
+    response = HTTP.get("https://api.spoonacular.com/recipes/random?tags=#{params[:diet]},#{params[:dish]}&number=1&apiKey=#{Rails.application.credentials.spoonacular[:api_key]}")
     @recipes = response.parse["recipes"]
     render "index.json.jb"
   end
+
+  # def index
+  #   response = HTTP.get("https://api.spoonacular.com/recipes/random?tags=vegetarian,dinner&number=1&apiKey=#{Rails.application.credentials.spoonacular[:api_key]}")
+  #   @recipes = response.parse["recipes"]
+  #   render "index.json.jb"
+  # end
 
 end
